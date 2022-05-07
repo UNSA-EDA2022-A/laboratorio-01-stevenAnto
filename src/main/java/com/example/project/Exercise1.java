2a7d8043d29b69c085c6fdab6aecbbf33ac5e2fd
stv <79427465+stevenAnto@users.noreply.github.com>
2022-05-07 10:18:21 -0500

    primeraSolucion

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,8 +15,48 @@ public class Exercise1 {
 	}
 
 	public String convertirBaseGabriel(int a) {
-
-		// TO DO
-		return "El numero proporcionado no esta en base Gabriel.";
-	}
+         if(verificar(a)){
+           int suma=0;
+           ArrayList<Integer> digitos= digitos(a);
+           for (int i=digitos.size()-1; i>0;i--){
+	   double d=digitos.get(i)*(Math.pow(2,i+1)-1);
+	   suma=(int)(suma+d);
+           }
+           // TO DO
+           return ""+suma;
+         }
+        else return "El numero proporcionado no esta en base Gabriel.";
+	}//
+	
+	  public static  boolean   verificar(int a){
+           int num=a;
+           boolean estado=true; //ultimo
+           int ultimo=0;
+           while(num>0){
+            int dig = num%10;
+            if(dig!=0&&estado){
+	    ultimo=dig;
+	    estado=false;
+	    dig=1;
+           }
+           if(!(ultimo==1||ultimo==2||ultimo==0))return false;
+           //System.out.println("el ultimo es"+ultimo);
+           num=num/10;
+           if(!(dig==1||dig==0))return false;
+         }
+         return true;
+        }
+	
+  public static ArrayList<Integer> digitos(int a){
+    ArrayList<Integer> digitos= new ArrayList<Integer>();
+    int num=a;
+    while(num>0){
+      int dig = num%10;
+      num = num/10;
+      digitos.add(dig);
+    }
+    return digitos;
+  }
+	
+	
 }
